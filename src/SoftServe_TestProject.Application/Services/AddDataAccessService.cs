979022ab583eca1df3603997e2d28b17af793d09
Земляков Сергій <seriyzemlyakov@gmail.com>
2024-12-11 20:17:35 +0200

    Added API and Application projects, and basic ProjectContext injection

---
+++ src/SoftServe_TestProject.Application/Services/AddDataAccessService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SoftServe_TestProject.Data;
+
+namespace SoftServe_TestProject.Application.Services
+{
+    public static class DataAccessService
+    {
+        public static IServiceCollection AddDataAccessService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ProjectContext>();
+
+            return services;
+        }
+    }
+}
